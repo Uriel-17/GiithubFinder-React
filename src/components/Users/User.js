97cd1005b39897cf4a9c5a23e46f9821610ba3f9
Uriel-17 <ug62005@gmail.com>
2021-07-18 13:58:19 -0700

    Display user repos

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner'; 
+import Repos  from '../repos/Repos'; 
 import PropTypes from 'prop-types'; 
 import { Link } from 'react-router-dom'; 
 
@@ -8,12 +9,15 @@ export class User extends Component {
 
   componentDidMount() {
     this.props.getUser(this.props.match.params.login); 
+    this.props.getUserRepos(this.props.match.params.login); 
   }
 
   static propTypes = {
     loading: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
   };
 
   render() {
@@ -34,7 +38,7 @@ export class User extends Component {
       hirable
     } = this.props.user; 
 
-    const { loading } = this.props; 
+    const { loading, repos } = this.props; 
 
     if(loading) {
       return <Spinner/>
@@ -98,6 +102,8 @@ export class User extends Component {
           <div className = 'badge badge-light'>Public Repos: {public_repos}</div>
           <div className = 'badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
+
+        <Repos repos = {repos} />
       </Fragment>
     )
   }
